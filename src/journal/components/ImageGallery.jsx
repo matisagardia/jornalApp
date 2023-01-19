fd36a4d9598d9ed453fdf250a7991d335e86ec77
matisagardia <matisagardia7@gmail.com>
2023-01-19 09:04:18 -0300

    imageGallery

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,10 +1,9 @@
+import { ImageList, ImageListItem } from "@mui/material";
 
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 
 export const ImageGallery = () => {
   return (
-    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={164}>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <img
@@ -12,7 +11,7 @@ export const ImageGallery = () => {
             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             alt={item.title}
             loading="lazy"
-          />
+          />  
         </ImageListItem>
       ))}
     </ImageList>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -8,11 +10,26 @@ import { NothingSelectedView } from "../views/NothingSelectedView"
 export const JournalPage = () => {
 
 
-  return (
+  return (  
     <JournalLayout>
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+    <IconButton 
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}>
+
+      <AddOutlined sx={{fontSize: 30}}/>
+
+    </IconButton>
 
     </JournalLayout>
   )
