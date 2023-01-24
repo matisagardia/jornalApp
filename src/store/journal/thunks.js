65181fbede77763117302f0952bd20c53545bb5f
matisagardia <matisagardia7@gmail.com>
2023-01-24 09:26:31 -0300

    render uploaded images

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -17,7 +17,7 @@ export const startNewNote = () => {
             title: '',
             body: '',
             date: new Date().getTime(),
-            imageUrls
+            imageUrls: []
         }
 
         const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
